f2129287670c93e90338b12f39847c617ad3351c
Quinten Steenhuis <qsteenhuis@gmail.com>
2023-04-28 14:30:23 -0400

    Got it working, wooho! need to work on layout + deployment next

--- app/pdfstats_migration.py
+++ app/pdfstats_migration.py
@@ -1,24 +1,33 @@
 import json
 import math
 import os
+from pathlib import Path
 import re
 from hashlib import sha256
 from typing import Tuple, Union, List
 
+import logging
 import pandas
 import textstat
 from fastapi import FastAPI, Request, File, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-# from rq import Queue
-# from rq.decorators import job
+from starlette.responses import Response
+from rq import Queue
+from rq.decorators import job
+import rq
+import pikepdf
 
 import formfyxer
 from formfyxer import lit_explorer
 from worker import conn
 
-# q = Queue(connection=conn)
+# Configure the logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+queue = Queue(connection=conn)
 
 app = FastAPI()
 
@@ -93,9 +102,50 @@ def get_pdf_from_dir(file_hash):
     return None
 
 
+def get_pdf_title_from_hash(file_hash:str) -> str:
+    f = get_pdf_from_dir(file_hash)
+    try:
+        the_pdf = pikepdf.open(f)
+        if hasattr(the_pdf.docinfo, "Title"):
+            title = str(the_pdf.docinfo.Title)
+        the_pdf.close()
+        return title
+    except:
+        path_without_extension = Path(f).stem
+        return path_without_extension.replace("-"," ").replace("_", " ")
+
+
 @app.get("/", response_class=HTMLResponse)
 async def upload_file(request: Request):
     return templates.TemplateResponse("ratemypdf.html", {"request": request})
+
+
+@job('default', connection=conn)
+def parse_form_job(to_path: str, filename: str, openai_creds: str, spot_token: str, tools_token: str, debug=False):
+    """
+    Run formfyxer.parse_form as a background job using RQ.
+
+    Args:
+        to_path (str): The path to the directory where the uploaded PDF file is stored.
+        filename (str): The name of the uploaded PDF file.
+        openai_creds (str): The OpenAI API credentials.
+        spot_token (str): The Spot API token.
+        tools_token (str): The Tools API token.
+        debug (bool): Controls whether the resulting stats will contain detailed information about each field
+    Saves:
+        stats.json: A JSON file containing the parsed form statistics, saved in the same directory as the PDF file.
+    """
+    stats = formfyxer.parse_form(
+        os.path.join(to_path, filename),
+        normalize=True,
+        debug=debug,
+        openai_creds=openai_creds,
+        spot_token=spot_token,
+        tools_token=tools_token,
+    )
+
+    with open(os.path.join(to_path, "stats.json"), "w") as stats_file:
+        stats_file.write(json.dumps(stats))
 
 
 @app.post("/")
@@ -110,7 +160,7 @@ async def process_file(file: UploadFile = File(...)) -> RedirectResponse:
     """
     if file.filename == "":
         raise HTTPException(status_code=400, detail="No file selected")
-    if file and allowed_file(file.filename):
+    if file and file.filename and allowed_file(file.filename):
         filename = file.filename
         file_content = await file.read()
         intermediate_dir = str(sha256(file_content).hexdigest())
@@ -123,21 +173,28 @@ async def process_file(file: UploadFile = File(...)) -> RedirectResponse:
         full_path = os.path.join(to_path, filename)
         with open(full_path, "wb") as write_file:
             write_file.write(file_content)
-        stats = formfyxer.parse_form(
-            full_path,
-            normalize=True,
-            debug=True,
+
+        logger.info(f"Writing file to disk: {filename}")
+
+        # Generate a unique identifier for the job
+        job = queue.enqueue(
+            parse_form_job,
+            to_path,
+            filename,
             openai_creds=os.environ.get("OPEN_AI"),
             spot_token=os.environ.get("SPOT_TOKEN"),
             tools_token=os.environ.get("TOOLS_TOKEN"),
+            debug=os.environ.get("RATEMYPDF_DEBUG"),
+            job_id=intermediate_dir,            
         )
-        with open(os.path.join(to_path, "stats.json"), "w") as stats_file:
-            stats_file.write(json.dumps(stats))
+
+        logger.info(f"Started job {job.id}")
+
         return RedirectResponse(url=f"/view/{intermediate_dir}", status_code=303)
     raise HTTPException(status_code=400, detail="Invalid file type")
 
 @app.get("/download/{file_hash}")
-async def download_file(file_hash: str) -> Union[FileResponse, RedirectResponse]:
+async def download_file(file_hash: str) -> Response:
     """Serve the uploaded PDF file for download.
 
     Args:
@@ -160,7 +217,7 @@ async def download_file(file_hash: str) -> Union[FileResponse, RedirectResponse]
 
 
 @app.get("/view/{file_hash}", response_class=HTMLResponse)
-async def view_stats(file_hash: str):
+async def view_stats(request: Request, file_hash: str):
     """Display the statistics of a previously uploaded and processed file.
 
     Args:
@@ -171,9 +228,31 @@ async def view_stats(file_hash: str):
     """
     if not (file_hash and valid_hash(file_hash)):
         raise HTTPException(status_code=400, detail="Invalid filename")
-    to_dir = os.path.join(UPLOAD_FOLDER, file_hash)
-    with open(os.path.join(to_dir, "stats.json")) as stats_file:
+    
+    variables = {
+        "request": request,
+        "title": get_pdf_title_from_hash(file_hash),
+        "file_hash": file_hash,
+    }
+
+    return templates.TemplateResponse("ratemypdf_stats.html", variables)
+
+@app.get("/job-status/{job_id}")
+async def get_job_status(request: Request, job_id: str):
+    job = rq.job.Job.fetch(job_id, connection=conn)
+    
+    if job.is_failed:
+        return {"status": "failed"}
+
+    if not job.is_finished:
+        return {"status": "pending"}
+
+    file_hash = job_id
+    # Load the stats from the stats.json file
+    stats_file_path = os.path.join(UPLOAD_FOLDER, file_hash, "stats.json")
+    with open(stats_file_path, "r") as stats_file:
         stats = json.loads(stats_file.read())
+
     metric_means = {
         "complexity score": 18.25398487,
         "time to answer": 49.266632,
@@ -216,7 +295,7 @@ async def view_stats(file_hash: str):
         passive_percent = 0
 
     vars = {
-        # "request": request,
+        "request": request,
         "stats": stats,
         "passive_percent": passive_percent,
         "title": stats.get("title", file_hash),
@@ -236,8 +315,21 @@ async def view_stats(file_hash: str):
         "pandas": pandas,
         "lit_explorer": lit_explorer,
     }
-    return templates.TemplateResponse("ratemypdf_stats.html", vars)
 
+
+    # Render the partial template with the stats
+    rendered_stats = templates.TemplateResponse(
+        "_stats_partial.html",
+        vars,
+        media_type="text/html"
+    )
+
+    # Return the rendered HTML as a string
+    return {"status": "finished", "rendered_stats": rendered_stats.body.decode()}
+
+@app.get("/loading_animation", response_class=HTMLResponse)
+async def loading_animation(request: Request):
+    return templates.TemplateResponse("loading_animation.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
